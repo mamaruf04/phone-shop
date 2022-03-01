bf401fa120d702add2946ab4eec1a09d6fdc3241
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-03-02 01:07:55 +0600

    more comment added

--- js/script.js
+++ js/script.js
@@ -1,84 +1,83 @@
-const searchGazette = () => {
+// take input value
+const searchDevice = () => {
     const searchField = document.getElementById('search-field');
     const UnknownSearchText = searchField.value;
     const searchText = UnknownSearchText.toLowerCase();
     // clear field
     searchField.value = '';
-
+    // data load
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.data))
 }
 
-const displaySearchResult = phones => {
+const displaySearchResult = devices => {
     const searchResult = document.getElementById('search-result');
     // clear previous search result
     searchResult.textContent ="";
-    const display20Phone = phones.splice(0, 20);
-    console.log(display20Phone);
-    display20Phone.forEach(phone => {
+    // get search results
+    const display20devices = devices.splice(0, 20);
+    display20devices.forEach(device => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card text-center h-100">
-                <img src="${phone.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
+                <img src="${device.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${phone.phone_name}</h5>
-                  <h6>${phone.brand}</h6>
+                  <h5 class="card-title">${device.phone_name}</h5>
+                  <h6>${device.brand}</h6>
                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
-                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-outline-light bg-danger w-25 mx-auto mb-4">Details</button>
+                <button onclick="loaddeviceDetails('${device.slug}')" class="btn btn-outline-light bg-danger w-25 mx-auto mb-4">Details</button>
             </div>
             `;
             searchResult.appendChild(div);
     });
 }
-
-const loadPhoneDetails = phoneId => {
-    // console.log(phoneId);
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+// load details data
+const loaddeviceDetails = deviceId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${deviceId}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhoneDetails(data.data));
 }
-
-const displayPhoneDetails = phoneId => {
-    console.log(phoneId);
+// display details data
+const displayPhoneDetails = deviceId => {
     let releaseDate = "";
-    if(`${phoneId.releaseDate}` === ""){
+    if(`${deviceId.releaseDate}` === ""){
         releaseDate = "Relese date not decleared yet.";
     }
     else{
-        releaseDate = `${phoneId.releaseDate}`
+        releaseDate = `${deviceId.releaseDate}`
     }
-    const phoneDetails = document.getElementById('phone-details');
-    phoneDetails.textContent = "";
+    const deviceDetails = document.getElementById('phone-details');
+    deviceDetails.textContent = "";
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
     <div class="card">
-        <img src="${phoneId.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
+        <img src="${deviceId.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
         <div class="card-body">
-            <h5 class="card-title fw-bold text-center">${phoneId.name}</h5>
-            <h6 class="card-title text-center">${phoneId.brand}</h6>
+            <h5 class="card-title fw-bold text-center">${deviceId.name}</h5>
+            <h6 class="card-title text-center">${deviceId.brand}</h6>
             <h6 class="card-title text-center"><span class="fw-bold">ReleaseDate: </span> ${releaseDate}</h6>
             <h6 class="text-center my-4 fw-bold">MainFeatures</h6>
-            <p><span class="fw-bold">ChipSet:</span> ${phoneId.mainFeatures.chipSet}</p>
-            <p><span class="fw-bold">Storage:</span> ${phoneId.mainFeatures.storage}</p>
-            <p><span class="fw-bold">DisplaySize:</span> ${phoneId.mainFeatures.displaySize}</p>
-            <p><span class="fw-bold">Memory:</span> ${phoneId.mainFeatures.memory}</p>
-            <p><span class="fw-bold">Memory:</span> ${phoneId.mainFeatures.memory}</p>
-            <p><span class="fw-bold">Sensors:</span> ${phoneId.mainFeatures.sensors}</p>
+            <p><span class="fw-bold">ChipSet:</span> ${deviceId.mainFeatures.chipSet}</p>
+            <p><span class="fw-bold">Storage:</span> ${deviceId.mainFeatures.storage}</p>
+            <p><span class="fw-bold">DisplaySize:</span> ${deviceId.mainFeatures.displaySize}</p>
+            <p><span class="fw-bold">Memory:</span> ${deviceId.mainFeatures.memory}</p>
+            <p><span class="fw-bold">Memory:</span> ${deviceId.mainFeatures.memory}</p>
+            <p><span class="fw-bold">Sensors:</span> ${deviceId.mainFeatures.sensors}</p>
             <h6 class="text-center my-4 fw-bold">Others</h6>
-            <p><span class="fw-bold">Bluetooth:</span> ${phoneId.others.Bluetooth}</p>
-            <p><span class="fw-bold">WLAN:</span> ${phoneId.others.WLAN}</p>
-            <p><span class="fw-bold">GPS:</span> ${phoneId.others.GPS}</p>
-            <p><span class="fw-bold">NFC:</span> ${phoneId.others.NFC}</p>
-            <p><span class="fw-bold">Radio:</span> ${phoneId.others.Radio}</p>
-            <p><span class="fw-bold">USB:</span> ${phoneId.others.USB}</p>
+            <p><span class="fw-bold">Bluetooth:</span> ${deviceId.others.Bluetooth}</p>
+            <p><span class="fw-bold">WLAN:</span> ${deviceId.others.WLAN}</p>
+            <p><span class="fw-bold">GPS:</span> ${deviceId.others.GPS}</p>
+            <p><span class="fw-bold">NFC:</span> ${deviceId.others.NFC}</p>
+            <p><span class="fw-bold">Radio:</span> ${deviceId.others.Radio}</p>
+            <p><span class="fw-bold">USB:</span> ${deviceId.others.USB}</p>
         </div>
     </div>
     `;
-    phoneDetails.appendChild(div);
+    deviceDetails.appendChild(div);
 }
