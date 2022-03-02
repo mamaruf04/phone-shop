ee903784cfb516d9fa154d175fd42f81f0437535
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-03-02 11:27:31 +0600

    error hendeler added

--- js/script.js
+++ js/script.js
@@ -3,19 +3,40 @@ const searchDevice = () => {
     const searchField = document.getElementById('search-field');
     const UnknownSearchText = searchField.value;
     const searchText = UnknownSearchText.toLowerCase();
-    // clear field
-    searchField.value = '';
-    // data load
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displaySearchResult(data.data))
+    if(searchText === ''){
+        const p = document.createElement('p');
+        p.innerHTML = `<p class="text-white text-center bg-danger p-1 w-50 mx-auto rounded">please search your product name to display</p>`;
+        const div = document.getElementById('error-message');
+        div.textContent = "";
+        div.appendChild(p);
+    }
+    else{
+        const div = document.getElementById('error-message');
+        div.textContent = "";
+        // clear field
+        searchField.value = '';
+        // data load
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
+    }
+    
 }
 
 const displaySearchResult = devices => {
     const searchResult = document.getElementById('search-result');
-    // clear previous search result
+    const deviceDetails = document.getElementById('phone-details');
+    // clear previous search result removed
     searchResult.textContent ="";
+    deviceDetails.textContent = "";
+    // error message for no result
+    if(Object.keys(devices).length == 0){
+        const p = document.createElement('p');
+        p.innerHTML = `<p class="text-white text-center bg-danger p-1 w-50 mx-auto rounded">No product found! please try again.</p>`;
+        const div = document.getElementById('error-message');
+        div.appendChild(p); 
+    }
     // get search results
     const display20devices = devices.splice(0, 20);
     display20devices.forEach(device => {
@@ -37,6 +58,10 @@ const displaySearchResult = devices => {
 }
 // load details data
 const loaddeviceDetails = deviceId => {
+    //remove all error message
+    const div = document.getElementById('error-message');
+    div.textContent = "";
+    // load details
     const url = `https://openapi.programming-hero.com/api/phone/${deviceId}`;
     fetch(url)
     .then(res => res.json())
@@ -44,14 +69,8 @@ const loaddeviceDetails = deviceId => {
 }
 // display details data
 const displayPhoneDetails = deviceId => {
-    let releaseDate = "";
-    if(`${deviceId.releaseDate}` === ""){
-        releaseDate = "Relese date not decleared yet.";
-    }
-    else{
-        releaseDate = `${deviceId.releaseDate}`
-    }
     const deviceDetails = document.getElementById('phone-details');
+    // previous data removed
     deviceDetails.textContent = "";
     const div = document.createElement('div');
     div.classList.add('card');
@@ -61,16 +80,16 @@ const displayPhoneDetails = deviceId => {
         <div class="card-body">
             <h5 class="card-title fw-bold text-center">${deviceId.name}</h5>
             <h6 class="card-title text-center">${deviceId.brand}</h6>
-            <h6 class="card-title text-center"><span class="fw-bold">ReleaseDate: </span> ${releaseDate}</h6>
+            <h6 class="card-title text-center"><span class="fw-bold">ReleaseDate: </span> ${deviceId.releaseDate ? deviceId.releaseDate : 'Relese date not decleared yet.'}</h6>
             <h6 class="text-center my-4 fw-bold">MainFeatures</h6>
-            <p><span class="fw-bold">ChipSet:</span> ${deviceId.mainFeatures.chipSet}</p>
+            <p><span class="fw-bold">ChipSet:</span> ${deviceId.mainFeatures.chipSet ? deviceId.mainFeatures.chipSet : 'Details Not Found'}</p>
             <p><span class="fw-bold">Storage:</span> ${deviceId.mainFeatures.storage}</p>
             <p><span class="fw-bold">DisplaySize:</span> ${deviceId.mainFeatures.displaySize}</p>
             <p><span class="fw-bold">Memory:</span> ${deviceId.mainFeatures.memory}</p>
             <p><span class="fw-bold">Memory:</span> ${deviceId.mainFeatures.memory}</p>
             <p><span class="fw-bold">Sensors:</span> ${deviceId.mainFeatures.sensors}</p>
             <h6 class="text-center my-4 fw-bold">Others</h6>
-            <p><span class="fw-bold">Bluetooth:</span> ${deviceId.others.Bluetooth}</p>
+            <p><span class="fw-bold">Bluetooth:</span> ${deviceId.others.Bluetooth }</p>
             <p><span class="fw-bold">WLAN:</span> ${deviceId.others.WLAN}</p>
             <p><span class="fw-bold">GPS:</span> ${deviceId.others.GPS}</p>
             <p><span class="fw-bold">NFC:</span> ${deviceId.others.NFC}</p>
